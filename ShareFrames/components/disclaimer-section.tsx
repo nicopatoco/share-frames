899e5a932bf98a2ce372b1f2a430714e25b4278f
Julio M Cruz <julio.cruz@eb-ms.net>
2024-03-23 22:19:09 -0400

    Update telegram

--- ShareFrames/components/disclaimer-section.tsx
+++ ShareFrames/components/disclaimer-section.tsx
@@ -12,7 +12,7 @@ export default function DisclaimerSection() {
         {/* <p className="mb-8 text-lg">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mauris nunc, ornare eget risus eget, vestibulum molestie lacus. Vestibulum pellentesque eget odio vel tristique. Nam suscipit varius ex nec aliquet. Aliquam erat volutpat. Integer ex ipsum, aliquam nec ultrices ut, pellentesque in magna. Sed tincidunt erat non lectus accumsan imperdiet. Quisque dui magna, accumsan a interdum vitae, sodales eu ligula. Cras eget lorem dolor.
         </p> */}
-        <Image src="/telegram.png" width={200} height={200} alt="Telegram" className="mx-auto" />
+        <Image src="https://bafybeifpwdujuhkmyt5evas75uwqogzg77kveqpqsqulebnl5qjghetnvm.ipfs.w3s.link/ShareFramesTelegram.png" width={200} height={200} alt="Telegram" className="mx-auto" />
         <div className="flex justify-center mt-12">
           <GetStartedButtonSection />
         </div>
--- ShareFrames/next.config.mjs
+++ ShareFrames/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+    images:{
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "bafybeifpwdujuhkmyt5evas75uwqogzg77kveqpqsqulebnl5qjghetnvm.ipfs.w3s.link",
+        }
+      ],
+    },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
